1fc1d4af69a3e49cfefffccf32175736342e7b26
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-29 19:58:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
-  return nil if array.nil?
-  return array[0] if array.length == 1
-  return array[0..-2].join(', ') + (',') + " and " + array[-1] if array.length > 2
-  if array.length == 2
+  if array.nil?
+  return nil
+  elsif array.length == 1
+    return array[0] 
+  elsif array.length > 2
+  return array[0..-2].join(', ') + (',') + " and " + array[-1] 
+  elsif array.length == 2
     array.insert(1, " and ")
     return array.join
   end
