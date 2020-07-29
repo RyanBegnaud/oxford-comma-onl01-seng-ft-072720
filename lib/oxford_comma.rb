2c7c86dacc499a964a3e3babb3d884bab42c1f1d
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-29 17:59:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  return nil if array.nil?
+  return array[0] if array.length == 1
+  return array[0..-2].join(', ') + (',') + " and " + array[-1] if array.length > 2
+  if array.length == 2
+    array.insert(1, " and ")
+    return array.join
+  end
 end
+
